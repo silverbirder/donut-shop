e5de151c0a530f07484a35588168fcbaada08081
shibamoto <silverbirder@gmail.com>
2025-06-25 19:01:34 +0900

    feat: add Donut type and initial donut data

---
+++ src/data/donut.data.ts
@@ -0,0 +1,177 @@
+export type Donut = {
+  name: string;
+  category: string;
+  imageUrl: string;
+};
+
+export const donuts: Donut[] = [
+  {
+    name: "もっちゅりん きなこ",
+    category: "もっちゅりん",
+    imageUrl:
+      "https://www.misterdonut.jp/m_menu/donut/images/407/product_1.jpg",
+  },
+  {
+    name: "もっちゅりん みたらし",
+    category: "もっちゅりん",
+    imageUrl:
+      "https://www.misterdonut.jp/m_menu/donut/images/408/product_1.jpg",
+  },
+  {
+    name: "もっちゅりん あずき",
+    category: "もっちゅりん",
+    imageUrl:
+      "https://www.misterdonut.jp/m_menu/donut/images/409/product_1.jpg",
+  },
+  {
+    name: "もっちゅりん 黒糖＆わらびもち",
+    category: "もっちゅりん",
+    imageUrl:
+      "https://www.misterdonut.jp/m_menu/donut/images/410/product_1.jpg",
+  },
+  {
+    name: "じゃがいもベーコンパイ",
+    category: "パイ",
+    imageUrl:
+      "https://www.misterdonut.jp/m_menu/donut/images/390/product_1.jpg",
+  },
+  {
+    name: "てりやきチキンパイ",
+    category: "パイ",
+    imageUrl:
+      "https://www.misterdonut.jp/m_menu/donut/images/392/product_1.jpg",
+  },
+  {
+    name: "さわやかトマトのチーズパイ",
+    category: "パイ",
+    imageUrl:
+      "https://www.misterdonut.jp/m_menu/donut/images/391/product_1.jpg",
+  },
+  {
+    name: "ポン・デ・リング",
+    category: "ポン・デ・リング",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/1/product_1.jpg",
+  },
+  {
+    name: "ポン・デ・黒糖",
+    category: "ポン・デ・リング",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/2/product_1.jpg",
+  },
+  {
+    name: "ポン・デ・ストロベリー",
+    category: "ポン・デ・リング",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/3/product_1.jpg",
+  },
+  {
+    name: "オールドファッション",
+    category: "オールドファッション",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/4/product_1.jpg",
+  },
+  {
+    name: "チョコファッション",
+    category: "オールドファッション",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/5/product_1.jpg",
+  },
+  {
+    name: "オールドファッション ハニー",
+    category: "オールドファッション",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/6/product_1.jpg",
+  },
+  {
+    name: "フレンチクルーラー",
+    category: "フレンチクルーラー",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/7/product_1.jpg",
+  },
+  {
+    name: "エンゼルフレンチ",
+    category: "フレンチクルーラー",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/8/product_1.jpg",
+  },
+  {
+    name: "ストロベリーカスタードフレンチ",
+    category: "フレンチクルーラー",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/9/product_1.jpg",
+  },
+  {
+    name: "ハニーディップ",
+    category: "イーストドーナツ",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/10/product_1.jpg",
+  },
+  {
+    name: "シュガーレイズド",
+    category: "イーストドーナツ",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/11/product_1.jpg",
+  },
+  {
+    name: "チョコリング",
+    category: "イーストドーナツ",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/12/product_1.jpg",
+  },
+  {
+    name: "ストロベリーリング",
+    category: "イーストドーナツ",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/13/product_1.jpg",
+  },
+  {
+    name: "エンゼルクリーム",
+    category: "イーストドーナツ",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/14/product_1.jpg",
+  },
+  {
+    name: "カスタードクリーム",
+    category: "イーストドーナツ",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/15/product_1.jpg",
+  },
+  {
+    name: "チョコレート",
+    category: "チョコレート",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/16/product_1.jpg",
+  },
+  {
+    name: "ダブルチョコレート",
+    category: "チョコレート",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/17/product_1.jpg",
+  },
+  {
+    name: "ゴールデンチョコレート",
+    category: "チョコレート",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/18/product_1.jpg",
+  },
+  {
+    name: "ココナツチョコレート",
+    category: "チョコレート",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/19/product_1.jpg",
+  },
+  {
+    name: "ハニーチュロ",
+    category: "バラエティドーナツ",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/20/product_1.jpg",
+  },
+  {
+    name: "エビグラタンパイ",
+    category: "パイ",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/32/product_1.jpg",
+  },
+  {
+    name: "ホット・セイボリーパイ BBQ フランクフルト",
+    category: "パイ",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/33/product_1.jpg",
+  },
+  {
+    name: "ホット・スイーツパイ りんご",
+    category: "パイ",
+    imageUrl: "https://www.misterdonut.jp/m_menu/donut/images/34/product_1.jpg",
+  },
+  {
+    name: "しっとりマフィン バター風味",
+    category: "マフィン",
+    imageUrl:
+      "https://www.misterdonut.jp/m_menu/donut/images/162/product_1.jpg",
+  },
+  {
+    name: "しっとりマフィン チョコ",
+    category: "マフィン",
+    imageUrl:
+      "https://www.misterdonut.jp/m_menu/donut/images/163/product_1.jpg",
+  },
+];
