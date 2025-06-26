f4839ffcbea0f775d59ef751215814724f100efd
shibamoto <silverbirder@gmail.com>
2025-06-26 21:16:58 +0900

    feat: add coffee data for Starbucks menu

---
+++ src/data/starbucks.data.ts
@@ -0,0 +1,342 @@
+import type { QuizItem } from ".";
+
+const Coffees: QuizItem[] = [
+  {
+    name: "スイート ミルクコーヒー",
+    reading: "すいーと みるくこーひー",
+    category: "コーヒー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785599772/4524785599772_11_s.jpg",
+  },
+  {
+    name: "ドリップ コーヒー",
+    reading: "どりっぷ こーひー",
+    category: "コーヒー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000018/4524785000018_11_s.jpg",
+  },
+  {
+    name: "カフェ ミスト",
+    reading: "かふぇ みすと",
+    category: "コーヒー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785062870/4524785062870_11_s.jpg",
+  },
+  {
+    name: "コールドブリュー コーヒー",
+    reading: "こーるどぶりゅー こーひー",
+    category: "コーヒー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785324732/4524785324732_11_s.jpg",
+  },
+  {
+    name: "コーヒー プレス",
+    reading: "こーひー ぷれす",
+    category: "コーヒー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785046856/4524785046856_11_s.jpg",
+  },
+  {
+    name: "ポットサービス",
+    reading: "ぽっとさーびす",
+    category: "コーヒー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000179/4524785000179_11_s.jpg",
+  },
+  {
+    name: "スターバックス® コーヒートラベラー",
+    reading: "すたーばっくす こーひーとらべらー",
+    category: "コーヒー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785410183/4524785410183_11_s.jpg",
+  },
+  {
+    name: "スターバックス ラテ",
+    reading: "すたーばっくす らて",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000223/4524785000223_11_s.jpg",
+  },
+  {
+    name: "ソイ ラテ",
+    reading: "そい らて",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785047396/4524785047396_11_s.jpg",
+  },
+  {
+    name: "アーモンドミルク ラテ",
+    reading: "あーもんどみるく らて",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785433106/4524785433106_11_s.jpg",
+  },
+  {
+    name: "オーツミルク ラテ",
+    reading: "おーつみるく らて",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785433021/4524785433021_11_s.jpg",
+  },
+  {
+    name: "カプチーノ",
+    reading: "かぷちーの",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000285/4524785000285_11_s.jpg",
+  },
+  {
+    name: "カフェ モカ",
+    reading: "かふぇ もか",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000254/4524785000254_11_s.jpg",
+  },
+  {
+    name: "ホワイト モカ",
+    reading: "ほわいと もか",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785034532/4524785034532_11_s.jpg",
+  },
+  {
+    name: "キャラメル マキアート",
+    reading: "きゃらめる まきあーと",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785019232/4524785019232_11_s.jpg",
+  },
+  {
+    name: "カフェ アメリカーノ",
+    reading: "かふぇ あめりかーの",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000315/4524785000315_11_s.jpg",
+  },
+  {
+    name: "エスプレッソ",
+    reading: "えすぷれっそ",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000346/4524785000346_11_s.jpg",
+  },
+  {
+    name: "トリプルエスプレッソ ラテ",
+    reading: "とりぷるえすぷれっそ らて",
+    category: "エスプレッソ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785457690/4524785457690_11_s.jpg",
+  },
+  {
+    name: "エスプレッソ アフォガート フラペチーノ®",
+    reading: "えすぷれっそ あふぉがーと ふらぺちーの",
+    category: "フラペチーノ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785344143/4524785344143_11_s.jpg",
+  },
+  {
+    name: "コーヒー フラペチーノ®",
+    reading: "こーひー ふらぺちーの",
+    category: "フラペチーノ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785165731/4524785165731_11_s.jpg",
+  },
+  {
+    name: "ダーク モカ チップ フラペチーノ®",
+    reading: "だーく もか ちっぷ ふらぺちーの",
+    category: "フラペチーノ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785166059/4524785166059_11_s.jpg",
+  },
+  {
+    name: "キャラメル フラペチーノ®",
+    reading: "きゃらめる ふらぺちーの",
+    category: "フラペチーノ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785165816/4524785165816_11_s.jpg",
+  },
+  {
+    name: "抹茶 クリーム フラペチーノ®",
+    reading: "まっちゃ くりーむ ふらぺちーの",
+    category: "フラペチーノ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785165892/4524785165892_11_s.jpg",
+  },
+  {
+    name: "バニラ クリーム フラペチーノ®",
+    reading: "ばにら くりーむ ふらぺちーの",
+    category: "フラペチーノ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785165939/4524785165939_11_s.jpg",
+  },
+  {
+    name: "マンゴー パッション ティー フラペチーノ®",
+    reading: "まんごー ぱっしょん てぃー ふらぺちーの",
+    category: "フラペチーノ",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785118751/4524785118751_11_s.jpg",
+  },
+  {
+    name: "ゆず シトラス & ユースベリー® ティー",
+    reading: "ゆず しとらす あんど ゆーすべりー てぃー",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785586925/4524785586925_11_s.jpg",
+  },
+  {
+    name: "ほうじ茶 & クラシックティー ラテ",
+    reading: "ほうじちゃ あんど くらしっくてぃー らて",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785557185/4524785557185_11_s.jpg",
+  },
+  {
+    name: "ゆず シトラス & パッション ティー",
+    reading: "ゆず しとらす あんど ぱっしょん てぃー",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785537170/4524785537170_11_s.jpg",
+  },
+  {
+    name: "ゆず シトラス & ティー",
+    reading: "ゆず しとらす あんど てぃー",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785461154/4524785461154_11_s.jpg",
+  },
+  {
+    name: "チャイ ティー ラテ",
+    reading: "ちゃい てぃー らて",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785034204/4524785034204_11_s.jpg",
+  },
+  {
+    name: "抹茶 ティー ラテ",
+    reading: "まっちゃ てぃー らて",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785106499/4524785106499_11_s.jpg",
+  },
+  {
+    name: "イングリッシュ ブレックファスト ティー ラテ",
+    reading: "いんぐりっしゅ ぶれっくふぁすと てぃー らて",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785304970/4524785304970_11_s.jpg",
+  },
+  {
+    name: "カモミール ティー ラテ",
+    reading: "かもみーる てぃー らて",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785305465/4524785305465_11_s.jpg",
+  },
+  {
+    name: "アール グレイ ティー ラテ",
+    reading: "あーる ぐれい てぃー らて",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785305076/4524785305076_11_s.jpg",
+  },
+  {
+    name: "ゼンクラウド ウーロン ティー ラテ",
+    reading: "ぜんくらうど うーろん てぃー らて",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785492820/4524785492820_11_s.jpg",
+  },
+  {
+    name: "アイスティー(ブラック)",
+    reading: "あいすてぃー ぶらっく",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785024748/4524785024748_11_s.jpg",
+  },
+  {
+    name: "アイスティー(パッション)",
+    reading: "あいすてぃー ぱっしょん",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785349193/4524785349193_11_s.jpg",
+  },
+  {
+    name: "イングリッシュ ブレックファスト",
+    reading: "いんぐりっしゅ ぶれっくふぁすと",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785305328/4524785305328_11_s.jpg",
+  },
+  {
+    name: "アール グレイ",
+    reading: "あーる ぐれい",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785305366/4524785305366_11_s.jpg",
+  },
+  {
+    name: "カモミール",
+    reading: "かもみーる",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785305571/4524785305571_11_s.jpg",
+  },
+  {
+    name: "ユースベリー®",
+    reading: "ゆーすべりー",
+    category: "ティー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785309104/4524785309104_11_s.jpg",
+  },
+  {
+    name: "ココア",
+    reading: "ここあ",
+    category: "その他",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000674/4524785000674_11_s.jpg",
+  },
+  {
+    name: "キャラメル クリーム",
+    reading: "きゃらめる くりーむ",
+    category: "その他",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785046900/4524785046900_11_s.jpg",
+  },
+  {
+    name: "ミルク",
+    reading: "みるく",
+    category: "その他",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000735/4524785000735_11_s.jpg",
+  },
+  {
+    name: "キッズ ココア",
+    reading: "きっず ここあ",
+    category: "キッズメニュー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000858/4524785000858_11_s.jpg",
+  },
+  {
+    name: "キッズ ミルク",
+    reading: "きっず みるく",
+    category: "キッズメニュー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785000865/4524785000865_11_s.jpg",
+  },
+  {
+    name: "be juicy ! kids （アップルミックス）",
+    reading: "びー じゅーしー きっず あっぷるみっくす",
+    category: "キッズメニュー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785216341/4524785216341_11_s.jpg",
+  },
+  {
+    name: "be juicy ! kids （オレンジミックス）",
+    reading: "びー じゅーしー きっず おれんじみっくす",
+    category: "キッズメニュー",
+    imageUrl:
+      "https://asset.menu.starbucks.co.jp/public/sku_images/4524785216334/4524785216334_11_s.jpg",
+  },
+];
+
+export { Coffees };
